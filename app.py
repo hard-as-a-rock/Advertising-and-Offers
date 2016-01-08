ee52cd70f4260c10d2eb5e85cf04d83125eeab5a
hard-as-a-rock <netugasa007@mail.ru>
2016-01-08 18:27:56 +0200

    Configured Flask-Login Manager

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -8,6 +9,9 @@ app.debug = True
 app.config.from_object('settings')
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 import main.models
 import main.views
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 SQLALCHEMY_DATABASE_URI = \
     "postgresql://role:role@localhost/role"
 
+
 try:
     from local_settings import *
 except ImportError:
